f3065c945ea4a7022515dc0010a5d1c24fed1639
R3bl0 <mlaskowski512@gmail.com>
2024-11-30 18:25:32 +0100

    collectable script and object

---
+++ Assets/Scripts/Collectable.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    public int count { get; private set; } = 0;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            count++;
+            Destroy(gameObject);
+        }
+    }
+}
