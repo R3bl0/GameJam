484d00fce77f1bdb496226983544c346acadc284
Kacper-Szczygiel <kacpi.szczygiel@gmail.com>
2024-12-01 00:17:26 +0100

    g

--- Assets/Scripts/PlayerState.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public static class PlayerState
-{
-    public static bool IsTopDown = false; // Informacja o trybie gry (true = top-down)
-    public static Vector3 PlayerPosition = Vector3.zero; // Aktualna pozycja gracza
-}
