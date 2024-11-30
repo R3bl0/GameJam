e66c233abf06ea38b717715de690347631d22562
R3bl0 <mlaskowski512@gmail.com>
2024-11-30 18:27:31 +0100

    static variable and collectables varriant

--- Assets/Scripts/Collectable.cs
+++ Assets/Scripts/Collectable.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class Collectable : MonoBehaviour
 {
-    public int count { get; private set; } = 0;
+    public static int count { get; private set; } = 0;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
